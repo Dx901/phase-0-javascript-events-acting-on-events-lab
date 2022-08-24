3787d67b8b3bc09cb982e624286a18122ac7ef47
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-08-24 07:40:55 +0300

    COmpleted Assignment

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function moveDodgerLeft() {
 
 
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
 
     if (left > 0) {
-        dodger.style.left = `${left + 1}px`;
+        dodger.style.left = `${left + 1}px`
     }
 }
 
